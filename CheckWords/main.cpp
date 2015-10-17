00eccd699e113fcb43c2ab400efee7899789637b
–Ш–ї—М—П –Ъ—Г–Ј—М–Љ–Є—З—С–≤ <mg108@bk.ru>
2015-10-17 19:27:50 +0300

    Initial Commit

---
+++ CheckWords/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CheckWords
+//
+//  Created by Илья Кузьмичев on 17.10.15.
+//  Copyright © 2015 Ilya Kuzmichev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
